545ebd9ca48122dc8d2e36d3fff9633d8f6ded56
Talson Slayman <44098012+TSlayman@users.noreply.github.com>
2022-02-21 23:58:29 +0300

    Update payaoay.py

--- payaoay.py
+++ payaoay.py
@@ -1,6 +1,6 @@
 import argparse
 import os.path
-
+import contextlib
 def nameShuffle (name):
     name = name.split() # Split the white spaces
     print(name[0][0]+name[1])
@@ -12,13 +12,22 @@ def nameShuffle (name):
     print(name[0][0]+'-'+name[1])
     print(name[0][0]+'_'+name[1])
 
+
 def is_valid_file(parser, arg): 
     if not os.path.exists(arg):
         parser.error("The file %s does not exist!" % arg)
     else:
         with open(inputFile, 'r') as usersList: # return an open file handle
-            for line in usersList:
-                nameShuffle(line)
+            if '-o':
+                with open(args.output, 'w+') as outputFile:
+                    for line in usersList:
+                        nameShuffle(line)
+                        with contextlib.redirect_stdout(outputFile):
+                            nameShuffle(line)
+            else:
+                for line in usersList:
+                    with contextlib.redirect_stdout(outputFile):
+                        nameShuffle(line)
 
 parser = argparse.ArgumentParser(description='A script to automate usernames pattern shuffling.')
 parser.add_argument("-o", "--output", help="Select output file.")
