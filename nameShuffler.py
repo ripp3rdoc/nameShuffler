53fc109b93b2db1a4fa37afc57f957ccbc1c7b6e
Talson Slayman <44098012+TSlayman@users.noreply.github.com>
2022-02-25 05:57:25 +0300

    Bug fixes + Added argument

--- nameShuffler.py
+++ nameShuffler.py
@@ -1,43 +1,65 @@
 import argparse
 import os.path
-import contextlib
+import sys
 
-print("""
-                                                                                        \n                             -_-/  ,,            /\\   /\\ ,,                             \n        _                   (_ /   ||           ||   ||  ||                             \n\\\\/\\\\  < \\, \\\\/\\\\/\\\\  _-_  (_ --_  ||/\\\\ \\\\ \\\\ =||= =||= ||  _-_  ,._-_    -_-_  \'\\\\/\\\\ \n|| ||  /-|| || || || || \\\\   --_ ) || || || ||  ||   ||  || || \\\\  ||      || \\\\  || ;\' \n|| || (( || || || || ||/    _/  )) || || || ||  ||   ||  || ||/    ||      || ||  ||/   \n\\\\ \\\\  \\/\\\\ \\\\ \\\\ \\\\ \\\\,/  (_-_-   \\\\ |/ \\\\/\\\\  \\\\,  \\\\, \\\\ \\\\,/   \\\\,  <> ||-\'   |/    \n                                     _/                                    |/    (      \n                                                                           \'      -_-   """)
-                                                                                        
-def nameShuffle (name):
-    name = name.split() # Split the white spaces
-    print(name[0][0]+name[1])
-    print(name[0][0]+'.'+name[1])
-    print(name[0]+'.'+name[1])
-    print(name[0]+name[1])
-    print(name[0]+'_'+name[1])
-    print(name[0]+'-'+name[1])
-    print(name[0][0]+'-'+name[1])
-    print(name[0][0]+'_'+name[1])
+# contextlib
+
+print(
+    """
+                                                                                        \n                             -_-/  ,,            /\\   /\\ ,,                             \n        _                   (_ /   ||           ||   ||  ||                             \n\\\\/\\\\  < \\, \\\\/\\\\/\\\\  _-_  (_ --_  ||/\\\\ \\\\ \\\\ =||= =||= ||  _-_  ,._-_    -_-_  \'\\\\/\\\\ \n|| ||  /-|| || || || || \\\\   --_ ) || || || ||  ||   ||  || || \\\\  ||      || \\\\  || ;\' \n|| || (( || || || || ||/    _/  )) || || || ||  ||   ||  || ||/    ||      || ||  ||/   \n\\\\ \\\\  \\/\\\\ \\\\ \\\\ \\\\ \\\\,/  (_-_-   \\\\ |/ \\\\/\\\\  \\\\,  \\\\, \\\\ \\\\,/   \\\\,  <> ||-\'   |/    \n                                     _/                                    |/    (      \n                                                                           \'      -_-   """
+)
 
 
-def is_valid_file(parser, arg): 
+def nameShuffle(name):
+    if "-d":
+        name = name.split()  # Split the white spaces
+        print(name[0][0] + name[1] + "@" + domain)
+        print(name[0][0] + "." + name[1] + "@" + domain)
+        print(name[0] + "." + name[1] + "@" + domain)
+        print(name[0] + name[1] + "@" + domain)
+        print(name[0] + "_" + name[1] + "@" + domain)
+        print(name[0] + "-" + name[1] + "@" + domain)
+        print(name[0][0] + "-" + name[1] + "@" + domain)
+        print(name[0][0] + "_" + name[1] + "@" + domain)
+    else:
+        name = name.split()  # Split the white spaces
+        print(name[0][0] + name[1])
+        print(name[0][0] + "." + name[1])
+        print(name[0] + "." + name[1])
+        print(name[0] + name[1])
+        print(name[0] + "_" + name[1])
+        print(name[0] + "-" + name[1])
+        print(name[0][0] + "-" + name[1])
+        print(name[0][0] + "_" + name[1])
+
+
+def is_valid_file(parser, arg):
     if not os.path.exists(arg):
         parser.error("The file %s does not exist!" % arg)
     else:
-        with open(inputFile, 'r') as usersList: # return an open file handle
-            if '-o':
-                with open(args.output, 'w+') as outputFile:
+        with open(inputFile, "r") as usersList:  # return an open file handle
+            if args.output is not None:
+                original_stdout = sys.stdout
+                with open(outputFile, "w+") as o:
                     for line in usersList:
+                        sys.stdout = o
                         nameShuffle(line)
-                        with contextlib.redirect_stdout(outputFile):
-                            nameShuffle(line)
+                        sys.stdout = original_stdout
+                        nameShuffle(line)
             else:
                 for line in usersList:
-                    with contextlib.redirect_stdout(outputFile):
-                        nameShuffle(line)
+                    nameShuffle(line)
 
-parser = argparse.ArgumentParser(description='A script to automate usernames pattern shuffling.')
+
+parser = argparse.ArgumentParser(
+    description="A script to automate usernames pattern shuffling."
+)
 parser.add_argument("-o", "--output", help="Select output file.")
-requiredNamed = parser.add_argument_group('required named arguments')
+parser.add_argument("-d", "--domain", help="Select a target domain.")
+requiredNamed = parser.add_argument_group("required named arguments")
 requiredNamed.add_argument("-f", "--file", help="Select users.txt file.", required=True)
-args = parser.parse_args()
+args, leftovers = parser.parse_known_args()
 outputFile = args.output
 inputFile = args.file
+domain = args.domain
 is_valid_file(parser, inputFile)
